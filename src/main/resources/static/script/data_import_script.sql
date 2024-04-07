65ad61edf1a16b74190adb5e08e5f6dab994e797
FeelingXD <wlals425315@gmail.com>
2024-04-07 22:55:08 +0900

    add : csv data import script
    -  csv data 파일을 import 한는데 작성한sql 스크립트 입니다.
    - LOAD DATA INFILE 해당 부분을 변경해서 사용해야합니다.

---
+++ src/main/resources/static/script/data_import_script.sql
@@ -0,0 +1,24 @@
+USE TMDB;
+
+LOAD DATA INFILE './movies_example_modified.csv' -- 경로를 변경해서 사용해주세요.
+INTO TABLE movie
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(@id, @title, @poster_path, @status, @genre, @overview, @budget, @revenue, @production_countries, @production_companies, @vote_count, @vote_average, @release_date, @language)
+SET
+  id = @id,
+  title = @title,
+  poster_path = @poster_path,
+  status = @status,
+  genre = @genre,
+  overview = @overview,
+  budget = CAST(REPLACE(@budget, 'E', '*10^') AS UNSIGNED),
+  revenue = CAST(REPLACE(@revenue, 'E', '*10^') AS UNSIGNED),
+  production_countries = @production_countries,
+  production_companies = @production_companies,
+  vote_count = @vote_count,
+  vote_average = @vote_average,
+  release_date = STR_TO_DATE(@release_date, '%Y-%m-%d'),
+  language = @language;

1f205a1d07fc3b292ef6a88832c69374ce60b76b
FeelingXD <wlals425315@gmail.com>
2024-04-09 03:35:35 +0900

    fix : N+1 problem
    - Movie - trailer의 관계로 인한 N+1 문제를 해결하기위해 fetch 조인으로 변경

--- src/main/java/com/example/mission/repository/MovieRepository.java
+++ src/main/java/com/example/mission/repository/MovieRepository.java
@@ -2,10 +2,13 @@ package com.example.mission.repository;
 
 import com.example.mission.model.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+public interface MovieRepository extends JpaRepository<Movie, Long> , MovieRepositoryCustom {
 
+    @Query("SELECT m FROM Movie m LEFT JOIN FETCH m.trailer")
     Optional<Movie> getMovieById(long id);
+
 }

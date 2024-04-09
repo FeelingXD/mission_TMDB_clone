edc718df34db03cc5777c1ee09b58b5000907819
FeelingXD <wlals425315@gmail.com>
2024-04-10 03:42:30 +0900

    feat : API_RESPONSE 응답통일

---
+++ src/main/java/com/example/mission/config/advise/RestControllerAdvise.java
@@ -0,0 +1,36 @@
+package com.example.mission.config.advise;
+
+import com.github.feelingxd.ApiResponse;
+import com.github.feelingxd.example.ExampleResponseCode;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice(
+        basePackages = "com.example.mission.controller"
+)
+public class RestControllerAdvise implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return MappingJackson2HttpMessageConverter.class.isAssignableFrom(converterType);
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body,
+                                  MethodParameter returnType,
+                                  MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request,
+                                  ServerHttpResponse response) {
+
+        return ApiResponse.builder()
+                .data(body)
+                .code(ExampleResponseCode.RESPONSE_SUCCESS)
+                .build();
+    }
+}

fdd1d11291f06caae4dbeaa2829f7164276ae1c4
FeelingXD <wlals425315@gmail.com>
2024-04-10 04:36:40 +0900

    update: ExcpetionCode
    - 명시적으로 변경 영화를 찾을 수 없습니다. -> ID 에 해당하는 영화를 찾을 수 없습니다.

--- src/main/java/com/example/mission/common/ErrorCode.java
+++ src/main/java/com/example/mission/common/ErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode implements Code {
-    MOVIE_NOT_FOUND(HttpStatus.BAD_REQUEST,"영화를 찾을 수 없습니다.");
+    MOVIE_NOT_FOUND(HttpStatus.BAD_REQUEST,"ID에 해당하는 영화를 찾을 수 없습니다.");
     private final HttpStatus status;
     private final String msg;
 }

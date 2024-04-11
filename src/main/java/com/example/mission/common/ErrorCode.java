6e74bf010989b27d21aabf253254da036ba37ce3
FeelingXD <wlals425315@gmail.com>
2024-04-12 04:42:25 +0900

    add : 404 응답 커스텀

--- src/main/java/com/example/mission/common/ErrorCode.java
+++ src/main/java/com/example/mission/common/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode implements Code {
-    MOVIE_NOT_FOUND(HttpStatus.BAD_REQUEST, "ID에 해당하는 영화를 찾을 수 없습니다.");
+    MOVIE_NOT_FOUND(HttpStatus.BAD_REQUEST, "ID에 해당하는 영화를 찾을 수 없습니다."),
+    WRONG_URL_REQUESTED(HttpStatus.BAD_REQUEST,"잘못된 요청 주소 입니다.");
     private final HttpStatus status;
     private final String msg;
 }

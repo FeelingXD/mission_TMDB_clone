4288697f7ee55750d0860ba7f28b8c798ee4531b
FeelingXD <wlals425315@gmail.com>
2024-04-14 18:31:28 +0900

    update : validation vote
    - 값검증을 위한 valid 관련 의존성이 추가 되었습니다.
    - vote 입력값에 대한 검증이 추가되었습니다.
    - 값 검증을 위한 어노테이션 및 조건이 추가 되었습니다.

--- src/main/java/com/example/mission/config/advise/ExceptionAdvise.java
+++ src/main/java/com/example/mission/config/advise/ExceptionAdvise.java
@@ -4,6 +4,8 @@ import com.example.mission.common.CustomException;
 import com.example.mission.common.ErrorCode;
 import com.github.feelingxd.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -17,5 +19,19 @@ public class ExceptionAdvise {
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<ApiResponse<Void>>  noHandlerFoundExceptionHandler(NoHandlerFoundException e){
         return ApiResponse.builder().code(ErrorCode.WRONG_URL_REQUESTED).toEntity();
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> processValidationError(MethodArgumentNotValidException e){
+        StringBuilder builder=new StringBuilder();
+        for (FieldError fieldError:e.getBindingResult().getFieldErrors()){
+            builder.append("[").append(fieldError.getField()).append("](은)는");
+            builder.append(fieldError.getDefaultMessage());
+            builder.append(String.format("입력된 값 [ %s ]",fieldError.getRejectedValue()));
+        }
+
+        return ApiResponse.builder()
+                .code(ErrorCode.VALIDATION_ERROR)
+                .data(builder)
+                .toEntity();
     }
 }
